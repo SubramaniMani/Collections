9b65ffdaf676e828d6bdb5dab4a6d54ee37ad1ab
SubramaniMani <142685216+SubramaniMani@users.noreply.github.com>
2023-08-19 23:00:58 +0530

    HashSet_program

---
+++ hashset_demo.java
@@ -0,0 +1,33 @@
+package collection;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+
+public class hashset_demo {
+	
+	public static void main(String[] args) {
+		
+		LinkedHashSet hs = new LinkedHashSet(); 
+		  hs.add(true);
+		  hs.add(10);
+		  hs.add(5.4f);
+		  hs.add("abcd");
+		  hs.add('c');
+		  
+		  System.out.println(hs);
+		  
+		  
+		  
+			/*
+			 * HashSet hs = new HashSet(); hs.add(true); hs.add(10); hs.add(5.4f);
+			 * hs.add("abcd"); hs.add('c');
+			 * 
+			 * System.out.println(hs);
+			 */
+		
+	}
+	
+	
+	}
+
+
