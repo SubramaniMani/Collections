4acaeaa58240102b853dad29bf97b954fb31b327
SubramaniMani <142685216+SubramaniMani@users.noreply.github.com>
2023-08-22 08:35:10 +0530

    Gegerics

---
+++ Genirics_Java.java
@@ -0,0 +1,36 @@
+package collection;
+
+import java.util.ArrayList;
+
+public class Genirics_Java {
+
+	public static void main(String[] args) {
+		
+		ArrayList<Integer> ob = new ArrayList<Integer>(); 
+		
+		ob.add(35);
+		ob.add(40);
+		ob.add(45);
+		ob.add(50);
+		
+		System.out.println(ob);
+		
+		ArrayList <String>ob1 = new ArrayList<String>();
+		
+		ob1.add("Subramani");
+		ob1.add("Suvetha");
+		ob1.add("Sumathi");
+		ob1.add("Sudha"); 
+		
+		System.out.println(ob1);
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
